96913c5732a5709a5b79b30d62f921f1ebc34f5b
pilarsilveyra <psilveyra@mail.austral.edu.ar>
2025-10-29 11:28:09 -0300

    agrego domain event interface

--- src/main/kotlin/com/printscript/execution/redis/SnippetsFormattingRulesUpdated.kt
+++ src/main/kotlin/com/printscript/execution/redis/SnippetsFormattingRulesUpdated.kt
@@ -2,4 +2,4 @@ package com.printscript.execution.redis
 
 import com.printscript.execution.dto.FormatterOptionsDto
 
-data class SnippetsFormattingRulesUpdated(val correlationalId: String, val snippetId: Long, val language: String, val version: String, val configText: String?, val configFormat: String?, val options: FormatterOptionsDto?, val attempt: Int = 0, val createdAt: Long = System.currentTimeMillis())
+data class SnippetsFormattingRulesUpdated(val correlationalId: String, val snippetId: Long, val language: String, val version: String, val configText: String?, val configFormat: String?, val options: FormatterOptionsDto?, val attempt: Int = 0, val createdAt: Long = System.currentTimeMillis()): DomainEvent
