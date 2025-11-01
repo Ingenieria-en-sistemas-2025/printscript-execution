c81fd5bdc8e65781a956213e8643093981556dca
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-11-01 09:56:30 -0300

    detekt

--- src/main/kotlin/com/printscript/execution/config/AppConfig.kt
+++ src/main/kotlin/com/printscript/execution/config/AppConfig.kt
@@ -8,27 +8,24 @@ import org.springframework.web.client.RestClient
 @Configuration
 class AppConfig(
     // Inyectamos el servicio M2M que obtiene y renueva el token
-    private val auth0TokenService: Auth0TokenService
+    private val auth0TokenService: Auth0TokenService,
 ) {
     // usado por Auth0TokenService para conseguir el token, pq no se tine que autenticar a si mismo
     // no necesita un token de autenticacion, pq lo esta pidiendo a /oauth/token
     @Bean("plainRestClient")
     fun plainRestClient(): RestClient = RestClient.create()
 
-
     @Bean("secureRestClient")
-    fun secureRestClient(): RestClient {
-        return RestClient.builder()
-            .requestInterceptor { request, body, execution ->
-                // Obtiene el token
-                val accessToken = auth0TokenService.getAccessToken()
+    fun secureRestClient(): RestClient = RestClient.builder()
+        .requestInterceptor { request, body, execution ->
+            // Obtiene el token
+            val accessToken = auth0TokenService.getAccessToken()
 
-                // Añadir el header de Autorizacion (Bearer Token)
-                request.headers.add("Authorization", "Bearer $accessToken")
+            // Añadir el header de Autorizacion (Bearer Token)
+            request.headers.add("Authorization", "Bearer $accessToken")
 
-                // Ejecuta la peticion
-                execution.execute(request, body)
-            }
-            .build()
-    }
+            // Ejecuta la peticion
+            execution.execute(request, body)
+        }
+        .build()
 }
